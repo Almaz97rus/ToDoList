ac864f35341944077d1b294036da2f19c9817ecc
Almaz Salimov <almaz-salimov@yandex.ru>
2020-06-14 02:37:35 +0300

    Добавьте файлы проекта.

---
+++ ToDoList/DBuse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+
+namespace ToDoList
+{
+    public class DBuse
+    {
+       private string path = @"C:\Users\User\source\repos\ToDoList\ToDoList\bin\Debug\DataBase.txt";
+
+        public StreamReader fileIn { get; set; }
+        public StreamWriter fileOut { get; set; }
+   
+        public List<string> ReadTextForDb(List<string> sst)
+        {         
+            fileIn = new StreamReader(path, Encoding.GetEncoding(1251));
+            string str;
+          
+            while ((str = fileIn.ReadLine()) != null) //пока поток не пуст
+            {
+                sst.Add(str);            
+            }
+
+            fileIn.Close();
+            return sst;       
+        }
+
+        public void AddTextForDb(string st)
+        {         
+            fileOut = new StreamWriter(path, true, Encoding.Default);
+            fileOut.WriteLine(st);
+            fileOut.Close();
+        }    
+    }
+}
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        DBuse dBuse = new DBuse();
+
+        public Form1()
+        {
+            InitializeComponent();      
+            textBox1.Tag = textBox1.Text = "Введите ФИО";
+
+            List<string> st = new List<string>();
+
+            dBuse.ReadTextForDb(st);
+
+            foreach (string s in st)
+            {
+                checkedListBox1.Items.Add(s);
+            }
+
+           // checkedListBox1.ForeColor = System.Drawing.Color.Red;       
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string st;
+            st = textBox1.Text.ToString();
+            checkedListBox1.Items.Add(st);
+            dBuse.AddTextForDb(st);
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkedListBox1.ClearSelected();          
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
