c2f05a1a56e2089fc7c955fe1ec998ef223a3e41
funkylen <funkylen@gmail.com>
2020-07-07 01:31:50 +0300

    07072020. IApp

---
+++ ToDoList/IApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToDoList.Items;
+
+namespace ToDoList
+{
+    // После каждого действия сохранять XML файл
+    interface IApp
+    {
+        List<Tasks> GetTasks();
+
+        bool Add(string TaskText);
+        bool Delete(int TaskId);
+        bool Complete(int TaskId);
+        bool Uncomplete(int TaskId);
+    }
+}
